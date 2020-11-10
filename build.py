60337d1b2f773ac8867669a6c776f48c530ffac3
lockieluke <sherlockluk@outlook.com>
2020-11-10 21:08:42 +0800

    Added .readline() to exec script

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ import os
 from colorama import Fore, Style, init
 
 print("Building...")
-print(os.popen('cd ' + os.getcwd() + ' && cd .. && tsc'))
+print(os.popen('cd ' + os.getcwd() + ' && cd .. && tsc').readline())
 print(Fore.GREEN + "Build Succeeded" + Fore.RESET)
