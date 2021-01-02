7e4209e93521ce47d283e5a809482b3ff3e33682
lockieluke <sherlockluk@outlook.com>
2021-01-02 13:12:48 +0800

    Clean repository - Rework on scripts

--- build.py
+++
@@ -1,6 +0,0 @@
-import os
-from colorama import Fore, Style, init
-
-print("Building...")
-print(os.popen('cd ' + os.getcwd() + ' && cd .. && tsc').readline())
-print(Fore.GREEN + "Build Succeeded" + Fore.RESET)
--- jdk.py
+++
@@ -1,9 +0,0 @@
-from colorama import Fore
-import requests
-import os
-
-download_url: str = 'https://download.java.net/java/GA/jdk15.0.1/51f4f36ad4ef43e39d0dfdbaf6549e32/9/GPL/openjdk-15.0.1_windows-x64_bin.zip'
-
-print(Fore.WHITE + "Attempting to download the latest OpenJDK for Zinc Native" + Fore.RESET)
-r = requests.get(download_url, allow_redirects=True)
-open(os.path.join(os.getcwd(), 'java', 'java.zip'), 'wb').write(r.content)
