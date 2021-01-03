611dd3c841c5b616ba64a784a2ed3398d30a7078
lockieluke <sherlockluk@outlook.com>
2021-01-03 17:40:39 +0800

    Fixed the issue that build doesn't start

--- build.py
+++ build.py
@@ -5,5 +5,5 @@ init(convert=True)
 
 proc: subprocess = subprocess.Popen('cmd.exe', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout, stderr = proc.communicate(b"yarn run build")
-print(stdout)
-print(Fore.RED + stderr + Fore.RESET)
+print(str(stdout))
+print(Fore.RED + str(stderr) + Fore.RESET)
